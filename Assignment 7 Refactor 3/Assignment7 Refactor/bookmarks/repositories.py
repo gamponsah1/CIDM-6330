21946aedeae45d549bb7d93d0090cc9d96f62012
Godfred Amponsah <144881720+gamponsah1@users.noreply.github.com>
2024-05-02 19:56:14 -0700

    Add files via upload

---
+++ Assignment 7 Refactor 3/Assignment7 Refactor/bookmarks/repositories.py
@@ -0,0 +1,21 @@
+from .models import Bookmark
+from django.db import transaction
+
+class BookmarkRepository:
+    def get_all_bookmarks(self):
+        return Bookmark.objects.all()
+
+    def add_bookmark(self, title, url, notes=''):
+        return Bookmark.objects.create(title=title, url=url, notes=notes)
+
+    def get_bookmark_by_id(self, id):
+        return Bookmark.objects.filter(id=id).first()
+
+    def update_bookmark(self, id, **kwargs):
+        Bookmark.objects.filter(id=id).update(**kwargs)
+
+    def delete_bookmark(self, id):
+         with transaction.atomic():
+            bookmark = self.get_bookmark_by_id(id)
+            if bookmark:
+                bookmark.delete()
