531b0622ee054bbb752e685530cbb49d052f40c6
Godfred Amponsah <144881720+gamponsah1@users.noreply.github.com>
2024-05-02 19:44:04 -0700

    Delete Final Project/elearning_platform directory

--- Final Project/elearning_platform/catalog/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class CreditCardForm(forms.Form):
-    name_on_card = forms.CharField(label='Name on Card', max_length=100)
-    card_number = forms.CharField(label='Card Number', max_length=16)
-    expiration_date = forms.DateField(label='Expiration Date')
-    cvv = forms.CharField(label='CVV', max_length=3)
